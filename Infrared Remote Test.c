04b95b377bc77ed3aa34e1a908ac60ebe988151d
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-03 21:51:38 -0400

    Create Infrared Remote Test.c

---
+++ Infrared Remote Test.c
@@ -0,0 +1,34 @@
+/*
+IR Remote Mode Values
+0 - No button
+1 - Red Up
+2 - Red Down
+3 - Blue Up
+4 - Blue Down
+5 - Red Up Blue Up
+6 - Red Up Blue Down
+7 - Red Down Blue Up
+8 - Red Down Blue Down
+9 - Beacon (large top button)
+10 - Red Up Red Down
+11 - Blue Up Blue Down
+*/
+
+
+task main()
+{
+	int IR_button_value = 0;
+
+	SensorType(S4) = sensorEV3_IRSensor;
+	wait1Msec(50);
+	SensorMode(S4) = modeEV3IR_Remote;
+	wait1Msec(50);
+
+
+	while(true){
+		IR_button_value = sensorValue(S4);
+		displayString(5, "%i", IR_button_value);
+	}
+
+
+}
