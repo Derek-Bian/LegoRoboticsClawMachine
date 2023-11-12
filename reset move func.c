6a63ee9f6eaafec5dd9f567d611dc26b3deae4bc
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-12 14:38:33 -0500

    Update reset move func.c

--- reset move func.c
+++ reset move func.c
@@ -6,7 +6,7 @@ Motors that move the axis are A and D, the motor that moves the cart is B
 this funcion moves the robot in the x-y axes without checking the motor encoder values
 */
 
-void manual_reset_move(int power){
+void manual_reset_move(int power = 10){
     switch(SensorValue(S1)){
         case 1:
             motor[motorA] = motor[motorD] = power;
