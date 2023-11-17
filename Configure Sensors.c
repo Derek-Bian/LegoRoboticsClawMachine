e4a72b6dfe2fa4cf244136ee5f1b967aa65ee392
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-17 14:26:55 -0500

    Update Configure Sensors.c

--- Configure Sensors.c
+++ Configure Sensors.c
@@ -7,12 +7,13 @@ IR1 Touch 2 color 3
 
 void configureSensors(){
     SensorType[S1] = sensorEV3_IRSensor;
-    wait1Msec(50);
+    wait1Msec(2000);
     SensorMode[S1] = modeEV3IR_Remote;
-    wait1Msec(50);
+    wait1Msec(2000);
     SensorType[S2] = sensorEV3_Touch;
     wait1Msec(50);
     SensorType[S3] = sensorEV3_Color;
     wait1Msec(50);
     SensorMode[S3] = modeEV3Color_Color;
+    wait1Msec(50);
 }
