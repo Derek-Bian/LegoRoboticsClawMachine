218fae40a9f45425a67ed818e218807c172d006f
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-17 15:49:28 -0500

    Update Limits.c

--- Limits.c
+++ Limits.c
@@ -1,19 +1,26 @@
-void limit(float &max_ad, float &max_b){
+int limit(float &max_ad, float &max_b){
     nMotorEncoder[motorA] = 0;
     nMotorEncoder[motorB] = 0;
 
     bool exit = false;
     while(!exit){
         manual_reset_move(20);
+
+        //exits if the user presses enter
         if(getButtonPress(buttonEnter)){
             while(!getButtonPress(buttonEnter)
             {}
             exit = true;
         }
+
+        //exits if inactive
+        if(time1[T1] > MAX_TIME)
+            return 1;
     }
 
     max_ad = nMotorEncoder[motorA];
     max_b = nMotorEncoder[motorB];
 
     return_to_origin(20);
+    return 0;
 }
