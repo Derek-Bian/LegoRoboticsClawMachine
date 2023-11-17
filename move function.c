d5918bae3b0dd0f714569833ea12af86252537de
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-17 15:45:58 -0500

    Update move function.c

--- move function.c
+++ move function.c
@@ -16,6 +16,9 @@ int move_during_game(int max_ad = 0, int max_b = 0, int power = 10){
     while (SensorValue(S1) != 0 && nMotorEncoder(motorA) < max_ad && nMotorEncoder(motorA) > 0 && nMotorEncoder(motorB) < max_b && nMotorEncoder(motorB) > 0)
     {}
 
+	//counted as active until user releases button
+	time1[T1] = 0;
+    
     motor[motorA] = motor[motorB] = motor[motorD] = 0;
     if(SensorValue(S1) == 0){
     	return 0;
