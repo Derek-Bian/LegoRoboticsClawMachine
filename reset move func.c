be81c91b517fdf45c6171fca6f174011e7e2aa5e
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-17 15:12:32 -0500

    Update reset move func.c

--- reset move func.c
+++ reset move func.c
@@ -7,16 +7,19 @@ this funcion moves the robot in the x-y axes without checking the motor encoder 
 */
 
 void manual_reset_move(int power = 10){
-    switch(SensorValue(S1)){
-        case 1:
-            motor[motorA] = motor[motorD] = power;
-        case 2:
-            motor[motorA] = motor[motorD] = -power;
-        case 3:
-            motor[motorB] = power;
-        case 4:
-            motor[motorB] = -power;
+    int sense = SensorValue(S1);
+	if(sense == 1){
+	    motor[motorA] = motor[motorD] = power;
     }
+    else if(sense == 2){
+    	motor[motorA] = motor[motorD] = -power;
+    }
+    else if(sense ==3){
+    	motor[motorB] = power;
+  	}
+  	else if(sense == 4){
+  		motor[motorB] = -power;
+  	}
 
     while(SensorValue(S1) != 0)
     {}
