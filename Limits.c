b9f5f3db1c1ca3c03dd7bc42cfb124aa993a1ba2
Derek-Bian <149850185+Derek-Bian@users.noreply.github.com>
2023-11-17 15:38:16 -0500

    Update Limits.c

--- Limits.c
+++ Limits.c
@@ -2,10 +2,15 @@ void limit(float &max_ad, float &max_b){
     nMotorEncoder[motorA] = 0;
     nMotorEncoder[motorB] = 0;
 
-    manual_reset_move(20);
-    
-    while(SensorValue(S1) != 9)
-    {}
+    bool exit = false;
+    while(!exit){
+        manual_reset_move(20);
+        if(getButtonPress(buttonEnter)){
+            while(!getButtonPress(buttonEnter)
+            {}
+            exit = true;
+        }
+    }
 
     max_ad = nMotorEncoder[motorA];
     max_b = nMotorEncoder[motorB];
