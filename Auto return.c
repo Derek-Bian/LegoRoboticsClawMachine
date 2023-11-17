17c847aeb911bb932941610f871c698a52f9ad0e
Hany25 <116024809+Hany25@users.noreply.github.com>
2023-11-17 15:32:10 -0500

    Update Auto return.c

--- Auto return.c
+++ Auto return.c
@@ -1,11 +1,12 @@
-void return_to_origin(int power){
-    while (nMotorEncoder[motorA] > 0){
-        motor[motorA] = motor[motorD] = -power;
-    }
+void a_return(int power){
+    motor[motorA] = motor[motorD] = -power;
+    while (nMotorEncoder[motorA] > 0)
+    {}
     motor[motorA] = motor[motorD] = 0;
-    while (nMotorEncoder[motorB] > 0){
-        motor[motorB] = -power;
-    }
+
+    motor[motorB] = -power;
+    while (nMotorEncoder[motorB] > 0)
+    {}
     motor[motorB] = 0;
 
 }
